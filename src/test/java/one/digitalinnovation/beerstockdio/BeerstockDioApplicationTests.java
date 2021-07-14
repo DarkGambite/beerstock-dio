19d17938f72c820516ff7a2d5ba2631b2b7734eb
Santos <santostizzo@gmail.com>
2021-07-14 20:27:16 -0300

    projeto final

---
+++ src/test/java/one/digitalinnovation/beerstockdio/BeerstockDioApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerstockdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
